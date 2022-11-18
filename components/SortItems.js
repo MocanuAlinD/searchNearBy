8258d79dfbe3f8083c8a377934b7875798209ce7
Mocanu Alin <alin_ngt@yahoo.com>
2022-11-18 21:46:29 +0200

    Restyled sort bar

--- components/SortItems.js
+++ components/SortItems.js
@@ -1,51 +1,51 @@
 import React from "react";
 import styles from "../styles/SortItems.module.scss";
 import { BsSortNumericUpAlt, BsSortNumericUp } from "react-icons/bs";
-import { AiOutlineCheckCircle } from "react-icons/ai";
+import { HiSortDescending,HiSortAscending } from "react-icons/hi";
 import { MdRadioButtonUnchecked } from "react-icons/md";
 
 const SortItems = () => {
   return (
     <div className={styles.container + " m-0 p-0 mb-3"}>
       <div className={styles.outerWrapper}>
+        <div className={styles.wrapper}>
+          <h3>Arata tot</h3>
+          <div className={styles.iconsContainer}>
+            <div className={styles.iconContainer + " " + styles.all}>
+              <input type="radio" name="all" checked={true} />
+            </div>
+          </div>
+        </div>
         <div className={styles.wrapper + " " + styles.tarif}>
-          <h3>Tarif: </h3>
+          <h3>Tarif</h3>
           <div className={styles.iconsContainer}>
-            <BsSortNumericUp />
-            <BsSortNumericUpAlt />
+            <div className={styles.iconContainer}>
+              <HiSortDescending className={styles.icon} />
+              <input type="radio" name="tarif" checked={true} />
+            </div>
+            <div className={styles.iconContainer}>
+              <HiSortAscending className={styles.icon} />
+              <input type="radio" name="tarif" />
+            </div>
           </div>
         </div>
         <div className={styles.wrapper}>
-          <h3>Disponibil peste program: </h3>
+          <h3>Disponibil peste program </h3>
           <div className={styles.iconsContainer}>
-            <AiOutlineCheckCircle />
-            <MdRadioButtonUnchecked />
+            <div className={styles.iconContainer + " " + styles.all}>
+              <input type="checkbox" name="all" />
+            </div>
           </div>
         </div>
         <div className={styles.wrapper}>
-          <h3>Urgente 24/7: </h3>
+          <h3>Urgente 24/7</h3>
           <div className={styles.iconsContainer}>
-            <AiOutlineCheckCircle />
-            <MdRadioButtonUnchecked />
+            <div className={styles.iconContainer + " " + styles.all}>
+              <input type="checkbox" name="all" />
+            </div>
           </div>
         </div>
       </div>
-      {/* <div
-        className={
-          styles.wrapper + " d-flex w-100 mx-2 p-2 justify-content-start"
-        }
-      >
-        <h3 className="me-3">Sorteaza: </h3>
-        <select className="w-75">
-          <optgroup label="Tarif"></optgroup>
-          <option value="asc">Crescator</option>
-          <option value="desc">Descrescator</option>
-          <optgroup label="Disponibil peste orele de program"></optgroup>
-          <option value="overtimeYes">Da</option>
-          <optgroup label="Urgente pe timp de noapte"></optgroup>
-          <option value="nightYes">Da</option>
-        </select>
-      </div> */}
     </div>
   );
 };
