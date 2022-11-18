cde69b04777fdab0fc6136a77c0c45f1a6709ae4
Mocanu Alin <alin_ngt@yahoo.com>
2022-11-18 15:27:23 +0200

    Styling the sort menu

--- components/SortItems.js
+++ components/SortItems.js
@@ -1,15 +1,49 @@
 import React from "react";
 import styles from "../styles/SortItems.module.scss";
+import { BsSortNumericUpAlt, BsSortNumericUp } from "react-icons/bs";
 
 const SortItems = () => {
   return (
     <div className={styles.container + " m-0 p-0"}>
-      <div className={styles.itemsContainer + " mx-2 my-3"}>
-        <div className={styles.wrapper}>First</div>
-        <div className={styles.wrapper}>Second</div>
-        <div className={styles.wrapper}>Third</div>
-        <div className={styles.wrapper}>Fourth</div>
+      <div className={styles.outerWrapper}>
+        <div className={styles.wrapper}>
+          <h3>Tarif: </h3>
+          <select>
+            <option value="asc">Crescator</option>
+            <option value="desc">Descrescator</option>
+          </select>
+        </div>
+        <div className={styles.wrapper}>
+          <h3>Disponibil peste program: </h3>
+          <select>
+            <option value="overtimeNo">Nu</option>
+            <option value="overtimeYes">Da</option>
+          </select>
+        </div>
+        <div className={styles.wrapper}>
+          <h3>Urgente 24/7: </h3>
+          <select>
+            <option value="nightNo">Nu</option>
+            <option value="nightYes">Da</option>
+          </select>
+        </div>
       </div>
+      {/* <div
+        className={
+          styles.wrapper + " d-flex w-100 mx-2 p-2 justify-content-start"
+        }
+      >
+        <h3 className="me-3">Sorteaza: </h3>
+        <select className="w-75">
+          <optgroup label="Tarif"></optgroup>
+          <option value="asc">Crescator</option>
+          <option value="desc">Descrescator</option>
+          <optgroup label="Disponibil peste orele de program"></optgroup>
+          <option value="overtimeYes">Da</option>
+          <optgroup label="Urgente pe timp de noapte"></optgroup>
+          <option value="nightYes">Da</option>
+        </select>
+      </div> */}
     </div>
   );
 };
