dd4b7bd3af54589e1fcc45536b28968b82f99683
Mocanu Alin <alin_ngt@yahoo.com>
2023-10-04 01:59:40 +0300

    Try_01 POST not working

--- pages/api/postData/index.js
+++ pages/api/postData/index.js
@@ -4,7 +4,8 @@ export default async function handler(req, res) {
   const { data } = JSON.parse(req.body);
   if (req.method === "POST") {
     try {
-      firebase.child(`Alin/${data.judet}`).push(data);
+      await firebase.child(`Alin/${data.judet}`).push(data);
+      // const delay = new Promise(() => setTimeout(3000,))
       res.json({ msg: `Date salvate cu succes in ${data.judet}` });
     } catch (error) {
       res.json({ error: `Datele nu s-au putut inregistra` });
