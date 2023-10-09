0908013b0f68177aae4dc0a0bd605a9440f77f53
Mocanu Alin <alin_ngt@yahoo.com>
2023-10-09 14:53:19 +0300

    Minor changes

--- TODO.js
+++ TODO.js
@@ -4,3 +4,26 @@
 // Remove "Toate judetele" in production
 // When clearing the search input, show all result without pressing again the search button
 // Clock on cards with available hours
+// In search input, put search text in the middle
+// Add to search card: Time online years, months, days, hours...
+// Make all possible text to romanian (șăâțî)
+
+
+
+// contact: email, phone
+// dataregister:
+// detalii
+// fullname
+// id
+// judet
+// orainceput
+// oraregister
+// oras
+// orasfarsit
+// pretMax
+// pretMin
+// tipjob
+// urgente: true/false
+// urgenteNoapte
+// ziinceput
+// zisfarsit
--- components/CardCautare.js
+++ components/CardCautare.js
@@ -2,10 +2,31 @@ import React from "react";
 import styles from "../styles/cardCautare.module.scss";
 import Link from "next/link";
 
+
+const namedMonths = {
+  1: "Ianuarie",
+  2: "Februarie",
+  3: "Martie",
+  4: "Aprilie",
+  5: "Mai",
+  6: "Iunie",
+  7: "Iulie",
+  8: "August",
+  9: "Septembrie",
+  10: "Octombrie",
+  11: "Noiembrie",
+  12: "Decembrie",
+}
+
+
 const CardCautare = ({ data, idx }) => {
   const gotoId = `/servicii/${data.judet}/${data.id}`;
+
+  const tempDate = new Date(data.dataregister)
+  const formatedDate = `${tempDate.getDate()} ${namedMonths[tempDate.getMonth() + 1]} ${tempDate.getFullYear()}`
+  console.log(namedMonths[tempDate.getMonth() + 1])
+
   return (
-    // <div className={styles.container + " m-0 p-0 px-1 mb-2 mx-md-1"}>
     <div className={styles.container + " px-1 mb-2 mx-md-1"}>
       <div className={styles.topWrapper}>
         <h6>{idx + 1}</h6>
@@ -24,10 +45,13 @@ const CardCautare = ({ data, idx }) => {
             <h6>{data.contact.phone}</h6>
           </div>
           <div className={styles.row}>
-            <p>Locatie:</p>
+            <p>Locație:</p>
             <h6>{data.judet + ", " + data.oras}</h6>
           </div>
-          <br />
+          {/* <br /> */}
+          <div className={styles.row}>
+            <p className={styles.disponibil}>Disponibil din: <b>{formatedDate}</b></p>
+          </div>
           <div className={styles.link}>
             <Link href={gotoId}>
               <a target="_blank">Vezi alte detalii</a>
