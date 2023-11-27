8d5f490a006aeb54646f1b57c6d09017805eb3a3
Mocanu Alin <alin_ngt@yahoo.com>
2023-11-27 16:10:38 +0200

    Passref to Link

--- components/BurgerMenu.js
+++ components/BurgerMenu.js
@@ -12,37 +12,37 @@ const BurgerMenu = () => {
       }
       style={{ transform: `rotateZ(${showMenu ? "0" : "90deg"})` }}
     >
-      <Link href="/servicii">
+      <Link href="/servicii" passHref>
         <span>
           <a className={styles.link + " m-0"}>Toate judetele</a>
         </span>
       </Link>
-      <Link href="/infofaq">
+      <Link href="/infofaq" passHref>
         <span>
           <a className={styles.link + " m-0"}>Info & FAQ</a>
         </span>
       </Link>
-      <Link href="/reclamatii">
+      <Link href="/reclamatii" passHref>
         <span>
           <a className={styles.link + " m-0"}>Reclamatii</a>
         </span>
       </Link>
-      <Link href="/contact">
+      <Link href="/contact" passHref>
         <span>
           <a className={styles.link + " m-0"}>Contact</a>
         </span>
       </Link>
-      <Link href="/login">
+      <Link href="/login" passHref>
         <span>
           <a className={styles.link + " m-0"}>Login & Sign up</a>
         </span>
       </Link>
-      <Link href="/inscriere">
+      <Link href="/inscriere" passHref>
         <span className="m-0 mt-5">
           <a className={styles.link}>Înregistrare gratuită</a>
         </span>
       </Link>
-      <Link href="/donatii">
+      <Link href="/donatii" passHref>
         <span>
           <a className={styles.link + " m-0"}>Donează pentru dezvoltare</a>
         </span>
