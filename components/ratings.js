ff6029cf1d992e59c07944ceb71e43886caf6034
Mocanu Alin <alin_ngt@yahoo.com>
2024-01-02 16:53:37 +0200

    Style added per user reviews from DB

--- components/ratings.js
+++ components/ratings.js
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "../styles/ratings.module.scss";
 import { FaStar } from "react-icons/fa";
 import RatingMedia from "../components/ratingMedia.js";
+import Stars from "../components/Stars.js";
 
 const Ratings = ({ userReviews }) => {
+  console.log(userReviews);
   const eachStar = {
     1: 0,
     2: 0,
@@ -40,45 +42,89 @@ const Ratings = ({ userReviews }) => {
 
   return (
     <div className={styles.ratingScore}>
-      <RatingMedia nos={media()} />
-      <div className={styles.allStars}>
-        {[...Array(5)]
-          .map((_, index) => {
+      <div className={styles.topContainer}>
+        <RatingMedia nos={media()} />
+        <div className={styles.allStars}>
+          {[...Array(5)]
+            .map((_, index) => {
+              return (
+                <div key={index}>
+                  <h4>{index + 1}</h4>
+                  <FaStar
+                    style={{
+                      color: "var(--color-yellow)",
+                      marginRight: ".5rem",
+                    }}
+                    size="20"
+                  />
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    id="slider"
+                    value={getPercent(index + 1)}
+                    readOnly
+                    style={{
+                      background: `linear-gradient(to right, var(--color-blue-darkish) ${getPercent(
+                        index + 1
+                      )}%, var(--color-light) ${getPercent(index + 1)}%)`,
+                      height: ".6rem",
+                    }}
+                  />
+                  <h4 className={styles.recenzii}>
+                    {eachStar[index + 1]}{" "}
+                    {eachStar[index + 1] == 1 ? "recenzie" : "recenzii"}
+                  </h4>
+                </div>
+              );
+            })
+            .reverse()}
+        </div>
+      </div>
+
+      <div className="justify-content-end pe-3">
+        <button>Lasa un review</button>
+      </div>
+      <div className={styles.bottomContainer}>
+        {userReviews &&
+          userReviews.map((item, index) => {
             return (
-              <div key={index}>
-                <h4>{index + 1}</h4>
-                <FaStar
-                  style={{
-                    color: "var(--color-yellow)",
-                    marginRight: ".5rem",
-                  }}
-                  size="20"
-                />
-                <input
-                  type="range"
-                  min="0"
-                  max="100"
-                  id="slider"
-                  value={getPercent(index + 1)}
-                  readOnly
-                  style={{
-                    background: `linear-gradient(to right, var(--color-wa) ${getPercent(
-                      index + 1
-                    )}%, var(--color-light) ${getPercent(index + 1)}%)`,
-                    height: ".6rem",
-                  }}
-                />
-                <h4 className={styles.recenzii}>
-                  {eachStar[index + 1]}{" "}
-                  {eachStar[index + 1] == 1 ? "recenzie" : "recenzii"}
-                </h4>
+              <div
+                key={index}
+                className={
+                  styles.userReviewContainer + " flex-column flex-md-row"
+                }
+              >
+                <div
+                  className={
+                    styles.userInfo + " align-items-md-start align-items-center"
+                  }
+                >
+                  <p>{item.numeReview}</p>
+                  <p>adaugat {item.postTime}</p>
+                  <span>
+                    <Stars nos={item.currentStar} />
+                  </span>
+                </div>
+                <div className={styles.userReview}>
+                  <p>&emsp;{item.sortRev}</p>
+                  <p>{item.longRev}</p>
+                </div>
               </div>
             );
-          })
-          .reverse()}
+          })}
       </div>
     </div>
   );
 };
 
 export default Ratings;
+
+// currentStar
+// id
+// judet
+// longRev
+// numeReview
+// oras
+// postTime
+// sortRev
