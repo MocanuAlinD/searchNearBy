1ddf2f9ac310a57415d4e8fe3f12f2d32e2562e7
Mocanu Alin <alin_ngt@yahoo.com>
2024-01-03 16:22:48 +0200

    Removed cautiserviciu page

--- pages/cautiserviciu.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Container from "../components/singleTags/elemetsCustom";
-
-const CautiServiciu = () => {
-  return <Container>Cauti serviciu</Container>;
-};
-
-export default CautiServiciu;
