b374bbc314b1f6a0369cba14609f85a0e895dc2f
Mocanu Alin <alin_ngt@yahoo.com>
2024-01-19 21:55:07 +0200

    svg button effect working

--- components/tags/Icon.js
+++ components/tags/Icon.js
@@ -1,95 +1,69 @@
 import React from "react";
+import styles from "../../styles/tags/Icon.module.scss";
 
-export const IconSearch = ({ size, color, stroke }) => {
+export const IconSearch = ({ size, color, sw }) => {
+  const clr = color ? color : "var(--color-light)";
+  const strokeW = sw ? sw : 20;
+  const sz = size ? size : "1.5rem";
   return (
-    <div
-      style={{
-        padding: "0",
-        margin: "0 .5rem",
-        width: size ? size : "1.35rem",
-        height: size ? size : "1.35rem",
-        display: "flex",
-      }}
-    >
-      <svg viewBox="0 0 1000 1000" fill="none">
-        <g transform-origin="center">
-          <circle
-            cx="659"
-            cy="330"
-            r="207"
-            stroke={color ? color : "var(--color-light)"}
-            stroke-width={stroke ? stroke : "30"}
-          />
-          <line
-            x1="517.678"
-            y1="503.678"
-            x2="230.678"
-            y2="852.678"
-            stroke={color ? color : "var(--color-light)"}
-            stroke-width={stroke ? stroke : "30"}
-            stroke-linecap="round"
-          />
-          <animateTransform
-            attributeName="transform"
-            type="scale"
-            dur="1.5s"
-            values="1;0.8;1"
-            keyTimes="0;0.5;1"
-            repeatCount="indefinite"
-          />
-        </g>
-      </svg>
-    </div>
+    <svg width={sz} viewBox="0 0 1000 1000" fill="none">
+      <g transform-origin="center">
+        <circle cx="659" cy="330" r="207" stroke={clr} stroke-width={strokeW} />
+        <line
+          x1="517.678"
+          y1="503.678"
+          x2="230.678"
+          y2="852.678"
+          stroke={clr}
+          stroke-width={strokeW}
+          stroke-linecap="round"
+        />
+        <animateTransform
+          attributeName="transform"
+          type="scale"
+          dur="2.5s"
+          values="1;0.8;1"
+          keyTimes="0;0.5;1"
+          repeatCount="indefinite"
+        />
+      </g>
+    </svg>
   );
 };
 
-export const IconDelete = ({ size, color, stroke }) => {
+export const IconDelete = ({ size, color, sw }) => {
+  const clr = color ? color : "white";
+  const strokeW = sw ? sw : 3;
+  const sz = size ? size : "1.5rem";
   return (
-    <div
-      style={{
-        padding: "0",
-        margin: "0 .5rem",
-        width: size ? size : "1.35rem",
-        height: size ? size : "1.35rem",
-        display: "flex",
-      }}
-    >
-      <svg viewBox="0 0 100 100" fill="none" preserveAspectRatio="none">
-        <g>
-          <path
-            id="contur"
-            d="M95 15 h-55 l-25 35 l25 35 h55 v-70"
-            stroke-linecap="round"
-            stroke-width="2"
-          />
-          <path
-            id="line1"
-            d="M45 30 l40 40"
-            stroke="white"
-            stroke-linecap="round"
-            stroke-width="2"
-          ></path>
-          <path
-            id="line2"
-            d="M45 70 l40 -40"
-            stroke="white"
-            stroke-linecap="round"
-            stroke-width="2"
-          ></path>
-        </g>
-        <style>
-          {`
-      #contur{
-        stroke: white;
-        stroke-width: 2;
-      }
-      #line1{
-        animation: line1 1s linear infinite;
-        stroke-dasharray: 57;
-        stroke-dashoffset: 57;
-      }
-      #line2{
-        animation: line2 1s linear infinite;
+    <svg width={sz} viewBox="0 0 100 100" fill="none">
+      <g>
+        <path
+          id="contur"
+          d="M95 15 h-55 l-25 35 l25 35 h55 v-70"
+          stroke-linecap="round"
+          stroke-width={strokeW}
+          stroke={clr}
+        />
+        <path
+          id="line1"
+          d="M45 30 l40 40"
+          stroke={clr}
+          stroke-linecap="round"
+          stroke-width={strokeW}
+        ></path>
+        <path
+          id="line2"
+          d="M45 70 l40 -40"
+          stroke={clr}
+          stroke-linecap="round"
+          stroke-width={strokeW}
+        ></path>
+      </g>
+      <style>
+        {`
+      #line1,#line2{
+        animation: line1 2s linear infinite;
         stroke-dasharray: 57;
         stroke-dashoffset: 57;
       }
@@ -104,20 +78,122 @@ export const IconDelete = ({ size, color, stroke }) => {
           opacity: 1;
         }
       }
-      @keyframes line2 {
-        from{
-          stroke-dasharray: 57;
-          stroke-dashoffset: 57;
+    `}
+      </style>
+    </svg>
+  );
+};
+
+export const IconPercent = ({ size, color, sw }) => {
+  const clr = color ? color : "white";
+  const strokeW = sw ? sw : 4;
+  const sz = size ? size : "1.5rem";
+  const circleLen = 62;
+  const percent = 63;
+  const speed = 200;
+  const duration = speed / circleLen;
+  return (
+    <svg width={sz} viewBox="-50 -50 100 100" fill="none">
+      <defs>
+        <linearGradient
+          x1="-30%"
+          y1="0%"
+          x2="100%"
+          y2="0%"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="rotate(45)"
+          id="lg"
+        >
+          <stop stopColor="white" offset="0%"></stop>
+          <stop stopColor="darkblue" offset="50%"></stop>
+        </linearGradient>
+      </defs>
+      <g>
+        <circle
+          id="circle"
+          cx="0"
+          cy="0"
+          r="10"
+          stroke="url(#lg)"
+          stroke-width={strokeW}
+        />
+        <text>{percent}%</text>
+      </g>
+
+      <style>
+        {`
+        #circle{
+          stroke-dasharray: ${circleLen};
+          stroke-dashoffset: ${circleLen};
+          animation: circle ${duration}s ease-out infinite;
+          transform: rotate(-90deg);
+        }
+        @keyframes circle {
+          50%,100%{
+            stroke-dashoffset: ${circleLen - (circleLen * percent) / 100};
+
+          }
+        }
+        text{
+          stroke: white;
+          font-size: .4rem;
+          font-weight: 800;
+          stroke-width: 0;
+          fill: var(--color-3-error);
+          text-anchor: middle;
+          dominant-baseline: middle;
+          animation: text ${duration}s ease-out infinite;
+          transform: translateY(20px);
           opacity: 0;
         }
-        to{
-          stroke-dashoffset: 0;
-          opacity: 1;
+        @keyframes text{
+          35%{
+            transform: translateY(0);
+            opacity: 1;
+          }
+          100%{
+            transform: translateY(0);
+            opacity: 1;
+          }
+          
         }
-      }
     `}
-        </style>
+      </style>
+    </svg>
+  );
+};
+
+export const IconTest = ({ size, color, sw }) => {
+  const clr = color ? color : "white";
+  const strokeW = sw ? sw : 4;
+  const sz = size ? size : "1.5rem";
+  const circleLen = 62;
+  const percent = 63;
+  const speed = 200;
+  const duration = speed / circleLen;
+  return (
+    <div className={styles.test}>
+      <div className={styles.div}></div>
+      <svg viewBox="0 0 100 100">
+        <defs>
+          <clipPath id={styles.clip}>
+            <path id={styles.path} d="M0 100 l100 -100 h-90 l-5 30z" />
+          </clipPath>
+        </defs>
       </svg>
     </div>
   );
 };
+// <img id={styles.image} src="icon48.png" alt="icons" />
+
+// <svg viewBox="0 0 100 100">
+//   <clipPath id="myClip">
+//     <circle cx="40" cy="35" r="35" />
+//   </clipPath>
+
+//   <path
+//     id="heart"
+//     d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
+//   />
+//   <use clip-path="url(#myClip)" href="#heart" fill="red" />
+// </svg>;
--- pages/test.js
+++ pages/test.js
@@ -9,6 +9,7 @@ import {
   setInitialStateReview,
 } from "../features/review/reviewSlice";
 import { getDatabase, ref, onValue } from "firebase/database";
+import { IconDelete, IconPercent, IconTest } from "../components/tags/Icon";
 
 const Test = () => {
   const [state, setState] = useState(0);
@@ -78,172 +79,127 @@ const Test = () => {
     console.log("Revs LIST: ", revs);
   };
 
+  const path = "M0 0 h1 v0.1 h-1z";
+
   return (
-    <Container className={styles.container}>
-      <button onClick={getAll}>get all</button>
-
-      <div className="d-flex justify-content-between">
-        {[...Array(5)].map((_, i) => {
-          const newI = i + 1;
-          return (
-            <div key={newI} className="w-100 m-0 p-0 m-2">
-              <h4
-                className="text-center m-0 p-0 fs-6"
-                style={{
-                  color: `${
-                    newI <= currentStar
-                      ? "var(--color-yellow)"
-                      : "var(--color-light)"
-                  }`,
-                }}
-              >
-                {newI}
-              </h4>
-              <input
-                type="radio"
-                checked={newI <= currentStar}
-                value={newI}
-                className={styles.input}
-                readOnly
-              />
-
-              <FaStar
-                onClick={() => changeSlider(newI)}
-                color={
-                  newI <= currentStar
-                    ? "var(--color-yellow)"
-                    : "var(--color-light)"
-                }
-                size="30"
-                className={styles.star}
-              />
-            </div>
-          );
-        })}
-      </div>
-
-      <div className={styles.ratingScore}>
-        {[...Array(5)]
-          .map((_, index) => {
-            return (
-              <div key={index}>
-                <h4>{index + 1}</h4>
-                <FaStar
-                  style={{
-                    color: "var(--color-yellow)",
-                    marginRight: ".5rem",
-                  }}
-                  size="25"
-                />
-                <input
-                  type="range"
-                  min="0"
-                  max="100"
-                  id="slider"
-                  value={getPercent(index + 1)}
-                  readOnly
-                  style={{
-                    background: `linear-gradient(to right, var(--color-blue-dark) ${getPercent(
-                      index + 1
-                    )}%, var(--color-light) ${getPercent(index + 1)}%)`,
-                  }}
-                />
-                <h4>{eachStar[index + 1]} recenzii</h4>
-              </div>
-            );
-          })
-          .reverse()}
-      </div>
-      <button
-        onClick={() => dispatch(setInitialStateReview())}
-        className="mt-5"
+    // <Container className={styles.container}>
+    <div className={styles.svgcontainer}>
+      <svg
+        viewBox="0 0 1 1"
+        stroke="red"
+        fill="none"
+        preserveAspectRatio="none"
       >
-        reset
-      </button>
-      <button>
-        <span></span>
-        Print state
-      </button>
-      <div className={styles.svg}>
-        <svg
-          width="100"
-          height="100"
-          viewBox="0 0 100 100"
-          fill="none"
-          preserveAspectRatio="none"
-          style={{ border: "1px solid red" }}
-        >
-          <g>
-            <rect id="bg" width="100" height="100" fill="#1A303A" />
-            <path
-              id="contur"
-              d="M95 15 h-55 l-25 35 l25 35 h55 v-70"
-              stroke-linecap="round"
-              stroke-width="2"
-            />
-            <path
-              id="line1"
-              d="M45 30 l40 40"
-              stroke="white"
-              stroke-linecap="round"
-              stroke-width="2"
-            ></path>
-            <path
-              id="line2"
-              d="M45 70 l40 -40"
-              stroke="white"
-              stroke-linecap="round"
-              stroke-width="2"
-            ></path>
-          </g>
-          <circle cx="45" cy="30" r="2" stroke="none" stroke-width="1" />
-          <style>
-            {`
-            #contur{
-              stroke: white;
-              stroke-width: 2;
-            }
-            #line1{
-              animation: line1 1s linear infinite;
-              stroke-dasharray: 57;
-              stroke-dashoffset: 57;
-            }
-            #line2{
-              animation: line2 1s linear infinite;
-              stroke-dasharray: 57;
-              stroke-dashoffset: 57;
-            }
-            @keyframes line1 {
-              from{
-                stroke-dasharray: 57;
-                stroke-dashoffset: 57;
-                opacity: 0;
-              }
-              to{
-                stroke-dashoffset: 0;
-                opacity: 1;
-              }
-            }
-            @keyframes line2 {
-              from{
-                stroke-dasharray: 57;
-                stroke-dashoffset: 57;
-                opacity: 0;
-              }
-              to{
-                stroke-dashoffset: 0;
-                opacity: 1;
-              }
-            }
-          `}
-          </style>
-        </svg>
-      </div>
-    </Container>
+        <defs>
+          <clipPath id="clip" clipPathUnits="objectBoundingBox">
+            <path id={styles.path} />
+          </clipPath>
+        </defs>
+      </svg>
+      <div className={styles.div}>ALIN</div>
+    </div>
+    // </Container>
   );
 };
 
 export default Test;
+
+// ==============================
+// <button onClick={getAll}>get all</button>
+
+//       <div className="d-flex justify-content-between">
+//         {[...Array(5)].map((_, i) => {
+//           const newI = i + 1;
+//           return (
+//             <div key={newI} className="w-100 m-0 p-0 m-2">
+//               <h4
+//                 className="text-center m-0 p-0 fs-6"
+//                 style={{
+//                   color: `${
+//                     newI <= currentStar
+//                       ? "var(--color-yellow)"
+//                       : "var(--color-light)"
+//                   }`,
+//                 }}
+//               >
+//                 {newI}
+//               </h4>
+//               <input
+//                 type="radio"
+//                 checked={newI <= currentStar}
+//                 value={newI}
+//                 className={styles.input}
+//                 readOnly
+//               />
+
+//               <FaStar
+//                 onClick={() => changeSlider(newI)}
+//                 color={
+//                   newI <= currentStar
+//                     ? "var(--color-yellow)"
+//                     : "var(--color-light)"
+//                 }
+//                 size="30"
+//                 className={styles.star}
+//               />
+//             </div>
+//           );
+//         })}
+//       </div>
+
+//       <div className={styles.ratingScore}>
+//         {[...Array(5)]
+//           .map((_, index) => {
+//             return (
+//               <div key={index}>
+//                 <h4>{index + 1}</h4>
+//                 <FaStar
+//                   style={{
+//                     color: "var(--color-yellow)",
+//                     marginRight: ".5rem",
+//                   }}
+//                   size="25"
+//                 />
+//                 <input
+//                   type="range"
+//                   min="0"
+//                   max="100"
+//                   id="slider"
+//                   value={getPercent(index + 1)}
+//                   readOnly
+//                   style={{
+//                     background: `linear-gradient(to right, var(--color-blue-dark) ${getPercent(
+//                       index + 1
+//                     )}%, var(--color-light) ${getPercent(index + 1)}%)`,
+//                   }}
+//                 />
+//                 <h4>{eachStar[index + 1]} recenzii</h4>
+//               </div>
+//             );
+//           })
+//           .reverse()}
+//       </div>
+//       <button
+//         onClick={() => dispatch(setInitialStateReview())}
+//         className="mt-5"
+//       >
+//         reset
+//       </button>
+//       <button>
+//         <span></span>
+//         Print state
+//       </button>
+// ==============================
+
+// <defs>
+//   <clipPath id="clip">
+//     <path id={styles.path} d={path} />
+//   </clipPath>
+// </defs>;
+
+// <div className={styles.svg + " border"}>
+//   <IconPercent size="20rem" />
+// </div>;
 
 // <animateTransform
 //   attributeName="transform"
