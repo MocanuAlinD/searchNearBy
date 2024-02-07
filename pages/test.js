f07ac8661ba7436500429205947eac6f6c9a7bfb
Mocanu Alin <alin_ngt@yahoo.com>
2024-02-07 12:29:15 +0200

    rubik cube

--- pages/test.js
+++ pages/test.js
@@ -1,17 +1,79 @@
 import React, { useState, useEffect } from "react";
-import SvgButton from "../components/tags/svgButton";
-import Romania from "../components/Romania";
 import styles from "../styles/pages/test.module.scss";
 
 const Test = () => {
-  const txt = () => {
-    const doc = document.getElementById("svg1");
-  };
-
   return (
-    <div className={styles.svgcontainer}>
-      <SvgButton onClick={txt}>Text</SvgButton>
-      <Romania />
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.one}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.two}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.three}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.four}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.five}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.six}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
