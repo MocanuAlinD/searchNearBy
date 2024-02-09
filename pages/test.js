2b7274464e7b1529be5696b93be0ac74cd89a5f6
Mocanu Alin <alin_ngt@yahoo.com>
2024-02-09 15:42:57 +0200

    changed start to flexstart

--- pages/test.js
+++ pages/test.js
@@ -2,18 +2,44 @@ import React, { useState, useEffect } from "react";
 import styles from "../styles/pages/test.module.scss";
 
 const Test = () => {
+  const moc = () => {
+    // const one = document.getElementById("c1");
+    // one.style.r = 100;
+    // one.style.fill = "red";
+    // one.classList = "";
+    // one.classList.add("mocanu");
+    const a = document.querySelectorAll("circle")
+    console.log(a)
+  };
+  const moc1 = () => {
+    // const one = document.getElementById("c1");
+    // one.style.r = 10;
+    // one.style.fill = "green";
+    // one.classList = "";
+    // one.classList.add("alin");
+  };
+
+  const dan = (item) => {
+    var items = document.querySelectorAll("UL.location__list LI A");
+    items.forEach(item => {
+      item.addEventListener("mouseover", itemMouseOver);
+      item.addEventListener("mouseout", itemMouseOut);
+    });
+  }
+
   return (
     <div className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <span className={styles.span + " " + styles.one}>1</span>
-          <span className={styles.span + " " + styles.two}>2</span>
-          <span className={styles.span + " " + styles.three}>3</span>
-          <span className={styles.span + " " + styles.four}>4</span>
-          <span className={styles.span + " " + styles.five}>5</span>
-          <span className={styles.span + " " + styles.six}>6</span>
-        </div>
-      </div>
+      <svg viewBox="0 0 1000 1000">
+        <circle
+          cx="100"
+          cy="100"
+          r="20"
+          fill="black"
+          onMouseEnter={() => moc()}
+          onMouseLeave={() => moc1()}
+        />
+        <circle id="c1" cx="250" cy="250" r="10" />
+      </svg>
     </div>
   );
 };
