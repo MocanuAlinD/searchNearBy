a80c9b273fe9ac2b6bd34c7b29d7cb93609fefba
Mocanu Alin <alin_ngt@yahoo.com>
2024-02-11 00:23:56 +0200

    css cards effect

--- pages/test.js
+++ pages/test.js
@@ -2,44 +2,22 @@ import React, { useState, useEffect } from "react";
 import styles from "../styles/pages/test.module.scss";
 
 const Test = () => {
-  const moc = () => {
-    // const one = document.getElementById("c1");
-    // one.style.r = 100;
-    // one.style.fill = "red";
-    // one.classList = "";
-    // one.classList.add("mocanu");
-    const a = document.querySelectorAll("circle")
-    console.log(a)
-  };
-  const moc1 = () => {
-    // const one = document.getElementById("c1");
-    // one.style.r = 10;
-    // one.style.fill = "green";
-    // one.classList = "";
-    // one.classList.add("alin");
-  };
-
-  const dan = (item) => {
-    var items = document.querySelectorAll("UL.location__list LI A");
-    items.forEach(item => {
-      item.addEventListener("mouseover", itemMouseOver);
-      item.addEventListener("mouseout", itemMouseOut);
-    });
-  }
+  const moc = () => {};
 
   return (
     <div className={styles.main}>
-      <svg viewBox="0 0 1000 1000">
-        <circle
-          cx="100"
-          cy="100"
-          r="20"
-          fill="black"
-          onMouseEnter={() => moc()}
-          onMouseLeave={() => moc1()}
-        />
-        <circle id="c1" cx="250" cy="250" r="10" />
-      </svg>
+      <div className="d-flex flex-row gap-2">
+        <button>click me</button>
+        <button>check</button>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.bottom}>bottom</div>
+          <div className={styles.top}>top</div>
+          <div className={styles.card1}>card1</div>
+          <div className={styles.card2}>card2</div>
+        </div>
+      </div>
     </div>
   );
 };
