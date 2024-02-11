0c8af1dc1e7893fff9d517216d1c7d48e7bb6e46
Mocanu Alin <alin_ngt@yahoo.com>
2024-02-11 22:59:37 +0200

    transform card 003 with details

--- components/CardCautareDesktop.js
+++ components/CardCautareDesktop.js
@@ -1,17 +1,46 @@
 import React from "react";
 import styles from "../styles/comps/cardCautareDesktop.module.scss";
+import Link from "next/link";
 
-const CardCautareDesktop = () => {
+const CardCautareDesktop = ({ data, key, idx, revs }) => {
+  const gotoId = `/servicii/${data.judet}/${data.id}`;
   return (
     <div className={styles.main}>
       <div className={styles.container}>
         <div className={styles.wrapper}>
-        <div className={styles.bottom}></div>
-        <div className={styles.top}></div>
-          <div className={styles.card1}>card1</div>
-          <div className={styles.card2}>card2</div>
-          <span className={styles.bottomFront}></span>
-          <span className={styles.bottomRight}></span>
+          <div className={styles.bottom}></div>
+          <div className={styles.top}></div>
+          <div className={styles.card1}>
+            <div className={styles.card1Content}>
+              <h6>Nume</h6>
+              <p>{data.fullname}</p>
+              <h6>Preturi</h6>
+              <p>
+                {data.pretMin} lei - {data.pretMax} lei
+              </p>
+              <h6>{data.tipjob}</h6>
+            </div>
+          </div>
+          <div className={styles.card2}>
+            <div className={styles.card2Content}>
+              <h6>Program</h6>
+              <p>
+                Zile: {data.ziinceput} - {data.zisfarsit}
+              </p>
+              <p>
+                Ore: {data.orainceput} - {data.orasfarsit}
+              </p>
+            </div>
+          </div>
+          <div className={styles.bottomFront}>
+            <div className={styles.link}>
+              <span>{data.tipjob}</span>
+              <Link href={gotoId}>
+                <a target="_blank">Vezi alte detalii</a>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.bottomRight}></div>
         </div>
       </div>
     </div>
