b0059ab0475775a3d7219809f241a2415aeb70c9
Mocanu Alin <alin_ngt@yahoo.com>
2024-02-19 12:31:24 +0200

    Layout with min width

--- components/LayoutPage.js
+++ components/LayoutPage.js
@@ -2,10 +2,11 @@ import React from "react";
 import BurgerMenu from "./BurgerMenu";
 import Navbar from "./Navbar";
 import ScrollWatcher from "../components/ScrollWatcher";
+import styles from "../styles/comps/Layout.module.scss";
 
 const LayoutPage = ({ children }) => {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className={styles.container}>
       <Navbar />
       <ScrollWatcher />
       {children}
