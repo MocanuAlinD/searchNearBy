28c9cb34e9bee1481e82d26077dd00a43f2982b8
Mocanu Alin <alin_ngt@yahoo.com>
2024-03-14 13:39:29 +0200

    Burger menu with svg animation on hover

--- components/tags/CustomIcon.js
+++ components/tags/CustomIcon.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import styles from "../../styles/comps/CustomImage.module.scss";
+import styles from "../../styles/comps/CustomIcon.module.scss";
 
 const CustomIcon = ({ src, size, text }) => {
   const sz = size ? size + "rem" : "2.5rem";
@@ -16,7 +16,25 @@ const CustomIcon = ({ src, size, text }) => {
           "--iconSrc": `url("/icons/${src ? src : "add"}.svg")`,
         }}
       ></div>
-      <h4 className={styles.text}>{text ? text : "No text here"}</h4>
+      <h4 className={styles.text}>
+        {text ? text : "No text here"}
+        <svg viewBox="0 0 100% 100%">
+          <defs>
+            <linearGradient id="myGradient" gradientTransform="rotate(30)">
+              <stop offset="0%" stopColor="#84fab0" />
+              <stop offset="100%" stopColor="#8fd3f4" />
+            </linearGradient>
+          </defs>
+          <rect
+            className={styles.rect}
+            // x="0%"
+            // y="0%"
+            width="100%"
+            height="100%"
+            fill="url(#myGradient)"
+          />
+        </svg>
+      </h4>
     </div>
   );
 };
