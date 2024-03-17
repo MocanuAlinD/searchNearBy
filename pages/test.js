0b954693a0e165ef6442ca4a102fbbf44cbb5965
Mocanu Alin <alin_ngt@yahoo.com>
2024-03-17 17:28:23 +0200

    Graph 02

--- pages/test.js
+++ pages/test.js
@@ -3,14 +3,19 @@ import { Container } from "../components/singleTags/elemetsCustom";
 import styles from "../styles/pages/test.module.scss";
 
 const Test = () => {
-  const values = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 50, 30, 3];
-  const topBottom = 2;
-  const size = values.length * 5 + values.length * 2 + topBottom;
+  const values = [
+    5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 50, 30, 3, 90, 20, 30, 40,
+    20, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 50, 30, 3, 90, 20, 30,
+    40, 100,
+  ];
   const max = Math.max(...values);
-  const finalValues = (x) => {
-    const one = ((100 * x) / max).toFixed(2);
-    return one;
-  };
+  const barHeight = 5;
+  const topBottom = barHeight / 2;
+  const size = values.length * barHeight + topBottom * values.length;
+  // const finalValues = (x) => {
+  //   const one = ((100 * x) / max).toFixed(2);
+  //   return one;
+  // };
 
   const colors = ["#d90429", "#f77f00", "#ffe5d9", "#b5e48c", "#7ae582"];
 
@@ -34,18 +39,17 @@ const Test = () => {
           id="graphSvg"
           viewBox={`0 0 ${size} ${size}`}
           preserveAspectRatio="none"
-          width="100%"
-          height={size}
+          width="10rem"
+          height={size * 2}
         >
           {values.map((item, index) => {
             const offset = index * 7 + topBottom;
-            console.log(finalValues(item));
             return (
               <rect
                 x="0"
                 y={offset}
                 width={item}
-                height="5"
+                height={barHeight}
                 fill={fillBar(item)}
               />
             );
